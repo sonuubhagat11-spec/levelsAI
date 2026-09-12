b27f7d53b68f2fd3b9e451706fcabfb1d5999ccd
Fluffy game <sonuubhagat11@gmail.com>
2026-09-12 21:34:03 +0530

    Add MIT License to LICENCE.py

---
+++ LICENCE.py
@@ -0,0 +1,22 @@
+import time
+print("MIT License")
+time.sleep(2)
+print("Copyright (c) 2026 sonuubhagat11-spec")
+
+print("Permission is hereby granted, free of charge, to any person obtaining a copy")
+print("of this software and associated documentation files (the Software), to deal")
+print("in the Software without restriction, including without limitation the rights")
+print("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell")
+print("copies of the Software, and to permit persons to persons to whom the")
+print("Software is furnished to do so, subject to the following conditions:")
+
+print("The above copyright notice and this permission notice shall be included in all")
+print("copies or substantial portions of the Software.")
+
+print("THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR")
+print("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,")
+print("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE")
+print("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER")
+print("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,")
+print("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE")
+print("SOFTWARE.")
